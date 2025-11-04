cbc1bd0ac187e8587c0b3ecb49cab44ccdacc634
Bradford Boubel <brbo5605@colorado.edu>
2025-11-03 21:47:23 -0700

    wa11 updates

--- wa11/wa11.js
+++ wa11/wa11.js
@@ -29,3 +29,61 @@ navToggle.addEventListener('click', showMenu);
 
 //Weather API
 
+let weatherBtn = document.querySelector('#get-weather-btn').addEventListener('click', getWeather);
+
+let currentWeather = {
+    location: "",
+    temp: "",
+    conditions: "",
+    humidity: "",
+    wind: ""
+}
+
+const apiKey = 'GNJYCDBQXGKVBPPAU93M9GRRV';
+
+async function getWeather() {
+    let location = document.getElementById('city-input').value;
+
+    if (location === "") {
+        alert("Please enter a city.");
+        return;
+    }
+
+    const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?unitGroup=us&key=" + apiKey + "&contentType=json";
+
+    try {
+        const response = await fetch(apiURL);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        const json = await response.json();
+        currentWeather.location = json["address"];
+        currentWeather.temp = json["currentConditions"]["temp"];
+        currentWeather.conditions = json["currentConditions"]["conditions"];
+        currentWeather.humidity = json["currentConditions"]["humidity"];
+        currentWeather.wind = json["currentConditions"]["windspeed"];
+
+        displayWeather(currentWeather);
+
+    } catch (err) {
+        console.log(err);
+        alert("Failed to get weather data");
+    }
+}
+
+function displayWeather(weather) {
+    const weatherContainer = document.querySelector('.weather-container');
+    const weatherText =
+        "Location: " + weather.location + "\n" +
+        "Temperature: " + weather.temp + " F\n" +
+        "Conditions: " + weather.conditions + "\n" +
+        "Humidity: " + weather.humidity + "%\n" +
+        "Wind Speed: " + weather.wind + " mph\n\n" +
+        "To get weather for another location, please refresh the page."
+    ;
+
+
+    weatherContainer.innerText = weatherText;
+}
